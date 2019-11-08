ce35133db20d13ebf4e3b64b3e411104f6763f03
tanialedemer <tanialedemer95@gmail.com>
2019-11-08 16:46:22 -0300

    proveedor editing finish

--- app/javascript/packs/bootstrap_custom.js
+++ app/javascript/packs/bootstrap_custom.js
@@ -11,3 +11,4 @@ import 'bootstrap/js/dist/tab'
 import 'bootstrap/js/dist/toast'
 import 'bootstrap/js/dist/tooltip'
 import 'bootstrap/js/dist/util'
+import 'bootstrap/dist/css/bootstrap.min.css';
--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -1,5 +1,5 @@
 class Proveedor < ApplicationRecord
   validates :nombre, presence: true
-  validates :ruc, presence: true
+  validates :ruc, presence: true, length: { maximum: 9 }
 
 end
